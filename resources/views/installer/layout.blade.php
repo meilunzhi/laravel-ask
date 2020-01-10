4aeaeed4f825eb2b358c846a97f37165cb10d2c7
meilunzhi <meilunzhi@gmail.com>
2020-01-10 13:59:31 +0800

    add

--- resources/views/installer/layout.blade.php
+++ resources/views/installer/layout.blade.php
@@ -22,7 +22,7 @@
 <body>
 <div class="header text-center">
   <h1>
-    <a href="http://www.tipask.com" class="logo">
+    <a href="http://dev.tipask.io" class="logo">
       <img src="{{ asset('/css/default/logo.png') }}" height="50" alt="Tipask" />
     </a>
   </h1>
@@ -45,7 +45,7 @@
 
 
 <footer class="main-footer text-center">
-  <strong>Copyright © 2010-{{ date("Y") }} <a href="http://www.tipask.com" target="_blank">tipask.com</a>.</strong> All rights reserved.
+  <strong>Copyright © 2010-{{ date("Y") }} <a href="http://dev.tipask.io" target="_blank">dev.tipask.io</a>.</strong> All rights reserved.
 </footer>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
