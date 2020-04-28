fd46d834eb356a08e168aa734ab8646b36b3c0f5
meilunzhi <meilunzhi@gmail.com>
2020-04-28 18:05:10 +0800

    update psr

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapFrontRoutes();
         //
     }
 
@@ -69,5 +70,16 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * @前台路由
+     * @return void
+     */
+    protected function mapFrontRoutes()
+    {
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/front.php'));
     }
 }
